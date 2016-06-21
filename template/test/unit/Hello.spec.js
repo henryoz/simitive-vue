05907f0bc6d9821c833e37ddcf6e5d7baf23b6ee
Henry Osadzinski <henry@osadzinski.co.uk>
2016-06-21 15:54:47 +0100

    small fixes

--- template/test/unit/Hello.spec.js
+++ template/test/unit/Hello.spec.js
@@ -9,6 +9,6 @@ describe('Hello.vue', () => {
       template: '<div><hello></hello></div>',
       components: { Hello }
     }).$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent).toBe('Hello World!');
+    expect(vm.$el.querySelector('.hello h1').textContent).toBe('Check your console!');
   });
 });
